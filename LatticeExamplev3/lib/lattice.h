00849cfe810063b4ad9966f2ea267035c00927ff
Nirav Shah <nirav.n.shah@intel.com>
2021-08-08 01:03:42 +0530

    Adding DLLEXPORTs for windows

--- LatticeExamplev3/lib/lattice.h
+++ LatticeExamplev3/lib/lattice.h
@@ -4,6 +4,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+#ifdef DLL_EXPORT
+#define DLLENTRY  __declspec(dllexport)   // export DLL information
+#elif  DLL_IMPORT
+#define DLLENTRY  __declspec(dllimport)   // import DLL information
+#else
+#define DLLENTRY
+#endif
+#else
+#define DLLENTRY
+#endif
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -11,33 +22,33 @@ extern "C" {
 /* A generic error type, 0 means success, nonzero values indicate an error */
 typedef int lattice_error_t;
     
-void simple();
+DLLENTRY void simple();
 
-int single_int(int a);
-int multiple_ints(int a, int b, int c);
-int different_ints(size_t s, uint32_t u, char c);
-int output_int(int a, int b, int* out);
+DLLENTRY int single_int(int a);
+DLLENTRY int multiple_ints(int a, int b, int c);
+DLLENTRY int different_ints(size_t s, uint32_t u, char c);
+DLLENTRY int output_int(int a, int b, int* out);
 
-lattice_error_t single_string(const char* s);
-lattice_error_t multiple_strings(const char* sa, const char* sb, const char* sc);
+DLLENTRY lattice_error_t single_string(const char* s);
+DLLENTRY lattice_error_t multiple_strings(const char* sa, const char* sb, const char* sc);
 
 /* Modifies the string, but not past its strlen() */
-lattice_error_t modify_string(char* s);
+DLLENTRY lattice_error_t modify_string(char* s);
 
 /* Takes a read-only input buffer buf with size len */
-lattice_error_t single_buffer(const void *buf, size_t len);
+DLLENTRY lattice_error_t single_buffer(const void *buf, size_t len);
 
 /* Fills an output buffer buf up to size len */
-lattice_error_t modify_buffer(int a, void *buf, size_t len);
+DLLENTRY lattice_error_t modify_buffer(int a, void *buf, size_t len);
 
 /* These are not buffers, but just pointers to random memory, and never read or written to */
-lattice_error_t just_pointers(const void* pa, void* pb);
+DLLENTRY lattice_error_t just_pointers(const void* pa, void* pb);
 
 /*
  * The output string out_name will be modified, but not past its strlen(). 
  * The buffers buf and out_buf will be read and written into, but not past len
  */
-lattice_error_t kitchen_sink(int a, const char* name, char* out_name, const void* buf, void* out_buf, size_t len);
+DLLENTRY lattice_error_t kitchen_sink(int a, const char* name, char* out_name, const void* buf, void* out_buf, size_t len);
 
 #ifdef __cplusplus
 }
